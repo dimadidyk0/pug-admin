ad69f72a9420447306c424a387fa1ab71ace3ed3
Dmytro Didyk <dmytro.didyk@betlab.com>
2019-02-23 13:51:11 +0200

    push develop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Develop</h1>
+        <h1>Develop 1</h1>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
