1d13be4c06a32fae671605c030b0b953ff65f1c1
annetlevitskaya <annetlevitskaya@gmail.com>
2019-02-23 14:58:27 +0300

    try to create conflict

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Develop 1</h1>
+        <h1>Develop Umary</h1>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -25,5 +25,7 @@ class App extends Component {
     );
   }
 }
+
+const pug = 'Umary';
 
 export default App;
